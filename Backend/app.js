0cf0bda7539c22566e25107b147da494fba165e6
DevendraNathLimbu <srijansilimbolimbu@gmail.com>
2026-01-11 20:53:07 +0545

    dockerizing app

--- Backend/app.js
+++ Backend/app.js
@@ -29,8 +29,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: ["http://localhost:3000",
-            "http://192.168.1.76:3000"],
+    origin: ["http://localhost:5173",
+            "http://192.168.1.76:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
